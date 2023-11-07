a65255960df0462943316eae9f8291d0782cd0e9
Daniel Kamenov <sorostmcoc@gmail.com>
2023-11-07 12:33:42 +0200

    Update firebase.jsx

--- softuni-react-app/src/firebase.jsx
+++ softuni-react-app/src/firebase.jsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCSiEKNmP6n-c00OiaXJYRTyAmpSx93fGQ",
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
 
 export default app;
